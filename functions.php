0d643fa8fc6b9417f5d447621b24e5b069d0ce96
Nick Colley <nickcolley7@gmail.com>
2024-08-13 04:26:20 +0100

    Auto load lazy blocks without wrappre

--- functions.php
+++ functions.php
@@ -167,5 +167,15 @@ function remove_wp_block_library_css(){
 }
 add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
 
-// Initialise custom blocks
-require(__DIR__ . '/blocks/lazyblock-section/index.php');
+/**
+ * Lazyblocks
+ */
+$block_directory_path = __DIR__ . '/blocks/lazyblock-';
+$blocks = glob($block_directory_path . '*', GLOB_ONLYDIR);
+foreach ($blocks as $block) {
+	$block_name = str_replace($block_directory_path, '', $block);
+	// Disable block frontend wrapper.
+	add_filter( "lazyblock/$block_name/frontend_allow_wrapper", '__return_false' );
+	// Initialise the block
+	require($block_directory_path . "$block_name/index.php");
+}
