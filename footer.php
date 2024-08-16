4bf531a4daf153a908d316aeaa0e931867134323
Nick Colley <nickcolley7@gmail.com>
2024-08-16 12:30:38 +0100

    Improve dark mode

--- footer.php
+++ footer.php
@@ -12,12 +12,18 @@
             <fieldset class="theme hidden-when-no-javascript" data-theme-select="">
                 <legend>Theme</legend>
                 <div class="field">
-                    <input class="visually-hidden" id="light" value="light" name="theme" type="radio" checked="">
-                    <label for="light">Light</label>
+                    <input class="visually-hidden" id="default" value="default" name="theme" type="radio">
+                    <label for="default">System default</label>
                 </div>
-                <div class="field">
-                    <input class="visually-hidden" id="dark" value="dark" name="theme" type="radio">
-                    <label for="dark">Dark</label>
+                <div class="field-group">
+                    <div class="field">
+                        <input class="visually-hidden" id="light" value="light" name="theme" type="radio" checked="">
+                        <label for="light">Light</label>
+                    </div>
+                    <div class="field">
+                        <input class="visually-hidden" id="dark" value="dark" name="theme" type="radio">
+                        <label for="dark">Dark</label>
+                    </div>
                 </div>
             </fieldset>
             <hr>
